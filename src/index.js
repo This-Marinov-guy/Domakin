6a20b53b1a313a0ec93f75dc842d86f152931f68
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-05-14 00:50:50 +0200

    modified:   public/index.html 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState, lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+
 import HomeV1 from './components/home-v1';
 import HomeV2 from './components/home-v2';
 import HomeV3 from './components/home-v3';
@@ -49,68 +51,65 @@ import OrderTracking from './components/order-tracking';
 import History from './components/history';
 
 
-class Root extends Component {
-    render() {
-        return (
-            <BrowserRouter basename={"/"}>
-                <Switch>
-                    <Route exact path="/" component={HomeV1} />
-                    <Route path="/home-v2" component={HomeV2} />
-                    <Route path="/home-v3" component={HomeV3} />
-                    <Route path="/home-v4" component={HomeV4} />
-                    <Route path="/home-v5" component={HomeV5} />
-                    <Route path="/home-v6" component={HomeV6} />
-                    <Route path="/home-v7" component={HomeV7} />
-                    <Route path="/home-v8" component={HomeV8} />
-                    <Route path="/home-v9" component={HomeV9} />
-                    <Route path="/home-v10" component={HomeV10} />
+const Root = () => {
+    return (
+        <BrowserRouter basename={"/"}>
+            <Switch>
+                <Route exact path="/" component={HomeV1} />
+                <Route path="/home-v2" component={HomeV2} />
+                <Route path="/home-v3" component={HomeV3} />
+                <Route path="/home-v4" component={HomeV4} />
+                <Route path="/home-v5" component={HomeV5} />
+                <Route path="/home-v6" component={HomeV6} />
+                <Route path="/home-v7" component={HomeV7} />
+                <Route path="/home-v8" component={HomeV8} />
+                <Route path="/home-v9" component={HomeV9} />
+                <Route path="/home-v10" component={HomeV10} />
 
-                    <Route path="/about" component={About} />
-                    <Route path="/service" component={Service} />
-                    <Route path="/service-details" component={ServiceDetails} />
-                    <Route path="/portfolio" component={Portfolio} />
-                    <Route path="/portfolio-v2" component={PortfolioV2} />
-                    <Route path="/portfolio-details" component={PortfolioDetails} />
-                    <Route path="/team" component={Team} />
-                    <Route path="/team-details" component={TeamDetails} />
-                    <Route path="/faq" component={Faq} />
-                    <Route path="/coming-soon" component={ComingSoon} />
-                    <Route path="/404" component={Error} />
-                    <Route path="/location" component={Location} />
-                    <Route path="/shop" component={Shop} />
-                    <Route path="/shop-grid" component={ShopGrid} />
-                    <Route path="/shop-left-sidebar" component={ShopLeftSidebar} />
-                    <Route path="/shop-right-sidebar" component={ShopRightSidebar} />
+                <Route path="/about" component={About} />
+                <Route path="/service" component={Service} />
+                <Route path="/service-details" component={ServiceDetails} />
+                <Route path="/portfolio" component={Portfolio} />
+                <Route path="/portfolio-v2" component={PortfolioV2} />
+                <Route path="/portfolio-details" component={PortfolioDetails} />
+                <Route path="/team" component={Team} />
+                <Route path="/team-details" component={TeamDetails} />
+                <Route path="/faq" component={Faq} />
+                <Route path="/coming-soon" component={ComingSoon} />
+                <Route path="/404" component={Error} />
+                <Route path="/location" component={Location} />
+                <Route path="/shop" component={Shop} />
+                <Route path="/shop-grid" component={ShopGrid} />
+                <Route path="/shop-left-sidebar" component={ShopLeftSidebar} />
+                <Route path="/shop-right-sidebar" component={ShopRightSidebar} />
 
-                    <Route path="/product-details" component={ProdductDetails} />
-                    {/* blog */}
-                    <Route path="/blog-grid" component={BlogGrid} />
-                    <Route path="/blog-left-sidebar" component={BlogLeftSidebar} />
-                    <Route path="/blog-right-sidebar" component={BlogRightSidebar} />
-                    <Route path="/blog" component={Blog} />
+                <Route path="/product-details" component={ProdductDetails} />
+                {/* blog */}
+                <Route path="/blog-grid" component={BlogGrid} />
+                <Route path="/blog-left-sidebar" component={BlogLeftSidebar} />
+                <Route path="/blog-right-sidebar" component={BlogRightSidebar} />
+                <Route path="/blog" component={Blog} />
 
 
-                    <Route path="/blog-details" component={BlogDetails} />
-                    <Route path="/contact" component={Contact} />
-                    <Route path="/cart" component={Cart} />
-                    <Route path="/checkout" component={Checkout} />
-                    <Route path="/my-account" component={MyAccount} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/register" component={Register} />
-                    <Route path="/add-listing" component={AddListing} />
-                    <Route path="/wishlist" component={Wishlist} />
-                    <Route path="/order-tracking" component={OrderTracking} />
-                    <Route path="/history" component={History} />
-                </Switch>
-            </BrowserRouter>
-        )
-    }
+                <Route path="/blog-details" component={BlogDetails} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/cart" component={Cart} />
+                <Route path="/checkout" component={Checkout} />
+                <Route path="/my-account" component={MyAccount} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+                <Route path="/add-listing" component={AddListing} />
+                <Route path="/wishlist" component={Wishlist} />
+                <Route path="/order-tracking" component={OrderTracking} />
+                <Route path="/history" component={History} />
+            </Switch>
+        </BrowserRouter>
+    )
 }
 
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <Root />
-);
+root.render(<Root />);
 
 // serviceWorker.unregister();
