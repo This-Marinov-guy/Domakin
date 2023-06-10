b617c355ead979a495ad157607d7d5d75a27887a
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2023-06-10 18:18:23 +0300

    modified:   src/components/global-components/navbar.js 	modified:   src/index.js

--- src/components/global-components/navbar.js
+++ src/components/global-components/navbar.js
@@ -37,6 +37,7 @@ const Navbar = () => {
 														<li><a href="#" className="dropdown-toggle"><span className="active-currency mr-5">Language </span></a>
 															<ul>
 																<li><button onClick={() => { dispatch(setLanguage('bg')); window.location.reload() }}>Български</button></li>
+																{/* fix the dispatch functionality in index.js as well  */}
 																{/* <li><button onClick={() => { dispatch(setLanguage('en')); window.location.reload() }}>English</button></li> */}
 																<li>Soon in ENG</li>
 															</ul>
