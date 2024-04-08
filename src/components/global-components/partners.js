785b728ee04c955ec6be5ef5535b48c98e065f74
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-04-08 20:14:50 +0300

    modified:   public/assets/img/partners/2.jpg 	new file:   public/assets/img/partners/3.png 	new file:   public/assets/img/partners/4.png 	modified:   src/components/global-components/partners.js

--- src/components/global-components/partners.js
+++ src/components/global-components/partners.js
@@ -11,14 +11,24 @@ const Partners = () => {
         <div className='container mt-40 mb-20'>
             <h2 className='text-center'>{script.partners[0]}</h2>
             <div className='row partners text-center'>
-                <div className='col-lg-6 col-md-6 col-sm-12'>
+                <div className='col-lg-3 col-md-6 col-sm-6'>
+                    <a href='https://bulgariansocietygro.nl/' target='_blank'>
+                        <img src={publicUrl + "assets/img/partners/2.jpg"} style={{ borderRadius: '50%' }} alt="Partner" className='partner-img' />
+                    </a>
+                </div>
+                <div className='col-lg-3 col-md-6 col-sm-6'>
                     <a href='https://bulgariansocietygro.nl/' target='_blank'>
                         <img src={publicUrl + "assets/img/partners/2.jpg"} alt="Partner" className='partner-img' />
                     </a>
                 </div>
-                <div className='col-lg-6 col-md-6 col-sm-12'>
-                    <a href='https://www.cooltravel.bg/' target='_blank'>
-                        <img src={publicUrl + "assets/img/partners/1.png"} alt="Partner" className='partner-img cool-travel' />
+                <div className='col-lg-3 col-md-6 col-sm-6'>
+                    <a href='https://studybuddy.bg/' target='_blank'>
+                        <img src={publicUrl + "assets/img/partners/3.png"} alt="Partner" className='partner-img' />
+                    </a>
+                </div>
+                <div className='col-lg-3 col-md-6 col-sm-6'>
+                    <a href='https://en.unify.bg/' target='_blank'>
+                        <img src={publicUrl + "assets/img/partners/4.jpg"} alt="Partner" className='partner-img' />
                     </a>
                 </div>
             </div>
